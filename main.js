578c02b4ebe4ba4bac38b73641c97a8643af88a8
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-01-30 02:11:28 -0700

    animacion de card

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 window.onload = function() {
 
+    const submitButton = document.getElementById('submit-btn');
 
+    const flipCardInner = document.getElementById('flip-card-inner');
+    
+
+    submitButton.addEventListener('click', function() {
+        flipCardInner.classList.toggle('flipped');
+    });
 }
