8adcd47bcf88cb152b3681b9fc1d512e2c09f0b0
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-02-09 19:56:39 -0700

    deploy

--- back-end/app/routes/paciente.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const paciente = require("../controladores/paciente");
-    let router = require("express").Router();
-
-    router.post("/", paciente.create);
-    router.post("/skip", paciente.result);
-    app.use("/api/paciente", router);
-  };
