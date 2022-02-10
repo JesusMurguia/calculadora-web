e199f26b314b14a44c47fa3db1f1cdd49078ee76
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-02-10 06:25:56 -0700

    no se ocupa

--- front-end/formSections/googleAuth.js
+++ front-end/formSections/googleAuth.js
@@ -9,10 +9,7 @@ export default class googleAuth{
         window.gapi.load('auth2', this.initSigninV2.bind(this));
     }
     initSigninV2(){
-        this.auth2 = gapi.auth2.getAuthInstance({
-            client_id: '30485697553-fi0lissi50apd8r26rl8rhg7hmkvafqo.apps.googleusercontent.com',
-            scope: 'profile'
-        });
+        this.auth2 = gapi.auth2.getAuthInstance();
         this.auth2.currentUser.listen(this.userChanged.bind(this));
         if (this.auth2.isSignedIn.get() == true) {
             this.auth2.signIn();
