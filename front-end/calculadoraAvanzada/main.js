b3acf156f198d5aab5f1a168ec575b7d8d6ecfc2
Ruben Cardoza <rubencardoza611@gmail.com>
2022-02-16 17:22:09 -0700

    se inicio la calculadora avanzada

---
+++ front-end/calculadoraAvanzada/main.js
@@ -0,0 +1,6 @@
+import formFirstPart from './formSections/formFirstPart.js';
+
+window.onload = function() {
+    // se crea el objeto formulario
+    const form1 = new formFirstPart(document.getElementById('formFirstPart'));
+}
---
+++ front-end/calculadoraAvanzada/services/fetchAPI.js
@@ -0,0 +1,28 @@
+export default class fetchAPI{
+    constructor(){
+    }
+
+    static postPaciente(paciente){
+        return fetch("https://borregotestnet.ddns.net/api/paciente", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*",
+            'Accept': 'application/json'
+          },
+           body: JSON.stringify(paciente)
+        })
+        .then(res => res.json());
+    }
+    static postPacienteSkip(paciente){
+      return fetch("https://borregotestnet.ddns.net/api/paciente/skip", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+         body: JSON.stringify(paciente)
+      })
+      .then(res => res.json());
+  }
+}
