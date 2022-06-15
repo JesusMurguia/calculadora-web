8f5dafde231dda631d8e912ba22ae319e2068041
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-06-14 18:28:19 -0700

    shmok

--- js/calculadora_basica/main.js
+++
@@ -1,6 +0,0 @@
-import formFirstPart from './formSections/formFirstPart.js';
-
-window.onload = function() {
-    // se crea el objeto formulario
-    const form1 = new formFirstPart(document.getElementById('formFirstPart'));
-}
